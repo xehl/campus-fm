d69d5b37743355eca2853fddc85890e25dd9ea9b
ehlee95 <eric95lee@gmail.com>
2022-11-21 11:55:03 -0500

    started work on DND

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Box, Grid, Container } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useEffect, useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import StationCard from "./components/stationcard";
 import Footer from "./components/footer";
 import Logo from "./components/logo";
@@ -12,6 +13,61 @@ import SelectorModal from "./components/selectormodal";
 import FaqModal from "./components/faqmodal";
 import StickyBar from "./components/stickybar";
 import ReactGA from "react-ga4";
+
+// count is passed as argument, offset as optional arg
+const getItems = (count, offset = 0) =>
+  // builds shallow array of length equal to count
+  // Array.from(arrayLike, (element, index) => { /* … */ })
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `item-${k + offset}`,
+    content: stations[k + offset],
+  }));
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+};
+
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+  destClone.splice(droppableDestination.index, 0, removed);
+
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
+
+  console.log(result);
+
+  return result;
+};
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  paddingRight: grid * 4,
+  margin: `0 0 ${grid}px 0`,
+  borderRadius: 7,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 500,
+});
 
 export default function App() {
   // MUI theme breakpoints
@@ -59,33 +115,33 @@ export default function App() {
     }
 
     // check and try to load 525/404 error stations every 5s (handleStall should catch stations that are loaded but stalled)
-    setInterval(() => {
-      const loadedStations = document.getElementsByClassName("audio-element");
-      for (let station of loadedStations) {
-        if (station.readyState === 0) {
-          const url = station.getAttribute("src");
-          let altered = "";
+    // setInterval(() => {
+    //   const loadedStations = document.getElementsByClassName("audio-element");
+    //   for (let station of loadedStations) {
+    //     if (station.readyState === 0) {
+    //       const url = station.getAttribute("src");
+    //       let altered = "";
 
-          // add/remove cors proxy and try again
-          if (url.startsWith("https://cors-proxy.elfsight.com/")) {
-            altered = url.substring(32);
-          } else {
-            altered = "https://cors-proxy.elfsight.com/" + url;
-          }
+    //       // add/remove cors proxy and try again
+    //       if (url.startsWith("https://cors-proxy.elfsight.com/")) {
+    //         altered = url.substring(32);
+    //       } else {
+    //         altered = "https://cors-proxy.elfsight.com/" + url;
+    //       }
 
-          station.setAttribute("src", "");
-          setTimeout(function () {
-            station.load(); // This stops the stream from downloading; basically forces it to load an empty file
-          }, 100);
-          station.setAttribute("src", altered);
-          station.load();
-          // console.log(station.getAttribute("src"));
-          // console.log(
-          //   "loading " + station.getAttribute("name") + station.readyState
-          // );
-        }
-      }
-    }, 5000);
+    //       station.setAttribute("src", "");
+    //       setTimeout(function () {
+    //         station.load(); // This stops the stream from downloading; basically forces it to load an empty file
+    //       }, 100);
+    //       station.setAttribute("src", altered);
+    //       station.load();
+    //       // console.log(station.getAttribute("src"));
+    //       // console.log(
+    //       //   "loading " + station.getAttribute("name") + station.readyState
+    //       // );
+    //     }
+    //   }
+    // }, 5000);
   }, []);
 
   // stores which station is currently playing
@@ -124,6 +180,46 @@ export default function App() {
     if (playStatic === true) staticSound.play();
     if (playStatic === false) staticSound.pause();
   }, [playStatic]);
+
+  const [droppableState, setDroppableState] = useState([
+    getItems(2),
+    getItems(10, 20),
+  ]);
+
+  function onDragEnd(result) {
+    const { source, destination } = result;
+
+    // dropped outside the list
+    if (!destination) {
+      return;
+    }
+
+    const sInd = +source.droppableId;
+    const dInd = +destination.droppableId;
+
+    if (sInd === dInd) {
+      const items = reorder(
+        droppableState[sInd],
+        source.index,
+        destination.index
+      );
+      const newState = [...droppableState];
+      newState[sInd] = items;
+      setDroppableState(newState);
+    } else {
+      const result = move(
+        droppableState[sInd],
+        droppableState[dInd],
+        source,
+        destination
+      );
+      const newState = [...droppableState];
+      newState[sInd] = result[sInd];
+      newState[dInd] = result[dInd];
+
+      setDroppableState(newState);
+    }
+  }
 
   return (
     <div className="App">
@@ -215,42 +311,112 @@ export default function App() {
             setPlaying={setPlaying}
           />
           <FaqModal faqOpen={faqOpen} handleFaqClose={handleFaqClose} />
-          <Container sx={{ width: "100%" }}>
-            <Grid
-              container
-              justifyContent="space-between"
-              rowSpacing={2}
-              columnSpacing={{ lg: 4, md: 3 }}
-            >
-              {selectedStations.map((station) => {
-                return (
-                  <Grid
-                    item
-                    md={12}
-                    lg={6}
-                    sx={{ width: "100%" }}
-                    key={station.id}
+          <Box sx={{ display: "flex" }}>
+            <Container sx={{ background: "blue" }}>
+              <div style={{ display: "flex" }}>
+                <DragDropContext onDragEnd={onDragEnd}>
+                  <Droppable
+                    key={0}
+                    droppableId={`0`}
+                    isDropDisabled={droppableState[0].length > 7 ? true : false}
                   >
-                    <StationCard
-                      callsign={station.call_sign}
-                      frequency={station.broadcast_frequency}
-                      college={station.college_name}
-                      audioURL={station.audio_url}
-                      collegeimage={station.college_image}
-                      setPlaying={setPlaying}
-                      stationObject={station}
-                      playing={playing}
-                      volume={volume}
-                      setPlayStatic={setPlayStatic}
-                      userPause={userPause}
-                      setUserPause={setUserPause}
-                    />
-                  </Grid>
-                );
-              })}
-            </Grid>
-          </Container>
-          <Footer handleSelectorModalOpen={handleSelectorModalOpen} />
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        style={getListStyle(snapshot.isDraggingOver)}
+                        {...provided.droppableProps}
+                      >
+                        {droppableState[0].map((item, index) => (
+                          <Draggable
+                            key={item.id}
+                            draggableId={item.id}
+                            index={index}
+                          >
+                            {(provided, snapshot) => (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                style={getItemStyle(
+                                  snapshot.isDragging,
+                                  provided.draggableProps.style
+                                )}
+                              >
+                                <StationCard
+                                  callsign={item.content.call_sign}
+                                  frequency={item.content.broadcast_frequency}
+                                  college={item.content.college_name}
+                                  audioURL={item.content.audio_url}
+                                  collegeimage={item.content.college_image}
+                                  setPlaying={setPlaying}
+                                  stationObject={item.content}
+                                  playing={playing}
+                                  volume={volume}
+                                  setPlayStatic={setPlayStatic}
+                                  userPause={userPause}
+                                  setUserPause={setUserPause}
+                                />
+                              </div>
+                            )}
+                          </Draggable>
+                        ))}
+                        {provided.placeholder}
+                      </div>
+                    )}
+                  </Droppable>
+                  <div style={{ height: 100, width: 100 }}></div>
+                  <div className="scroller">
+                    <Droppable key={1} droppableId={`1`}>
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          style={getListStyle(snapshot.isDraggingOver)}
+                          {...provided.droppableProps}
+                        >
+                          {droppableState[1].map((item, index) => (
+                            <Draggable
+                              key={item.id}
+                              draggableId={item.id}
+                              index={index}
+                            >
+                              {(provided, snapshot) => (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  style={getItemStyle(
+                                    snapshot.isDragging,
+                                    provided.draggableProps.style
+                                  )}
+                                >
+                                  <StationCard
+                                    callsign={item.content.call_sign}
+                                    frequency={item.content.broadcast_frequency}
+                                    college={item.content.college_name}
+                                    audioURL={item.content.audio_url}
+                                    collegeimage={item.content.college_image}
+                                    setPlaying={setPlaying}
+                                    stationObject={item.content}
+                                    playing={playing}
+                                    volume={volume}
+                                    setPlayStatic={setPlayStatic}
+                                    userPause={userPause}
+                                    setUserPause={setUserPause}
+                                  />
+                                </div>
+                              )}
+                            </Draggable>
+                          ))}
+                          {provided.placeholder}
+                        </div>
+                      )}
+                    </Droppable>
+                  </div>
+                </DragDropContext>
+              </div>{" "}
+            </Container>
+          </Box>
+          {/* <Footer handleSelectorModalOpen={handleSelectorModalOpen} /> */}
           <audio
             className="staticAudio"
             preload="auto"
--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ ReactGA.initialize("UA-239891642-1");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
