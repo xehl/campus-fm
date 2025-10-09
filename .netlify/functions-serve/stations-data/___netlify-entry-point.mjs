8f5c9ca7c5e7a6447c95152629abcb8823c4d39f
Eric Lee <ericlee@Erics-MacBook-Pro.local>
2025-10-09 09:32:11 -0700

    revert: remove netlify functions and API, return to original state

---
+++ .netlify/functions-serve/stations-data/___netlify-entry-point.mjs
@@ -0,0 +1,1 @@
+import * as bootstrap from './___netlify-bootstrap.mjs';import * as func from './netlify/functions/stations-data.cjs';const funcModule = typeof func.default === "function" ? func : func.default;export const handler = bootstrap.getLambdaHandler(funcModule)
