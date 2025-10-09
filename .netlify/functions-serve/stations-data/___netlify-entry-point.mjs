e4e3e4131aeafd3f1d4f48e28255363e5b808d07
Eric Lee <ericlee@Erics-MacBook-Pro.local>
2025-10-09 09:32:40 -0700

    clean up remaining netlify files

--- .netlify/functions-serve/stations-data/___netlify-entry-point.mjs
+++
@@ -1,1 +0,0 @@
-import * as bootstrap from './___netlify-bootstrap.mjs';import * as func from './netlify/functions/stations-data.cjs';const funcModule = typeof func.default === "function" ? func : func.default;export const handler = bootstrap.getLambdaHandler(funcModule)
