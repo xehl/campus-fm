e8671081e9a6983773cb4a90521d5f5a77d8746e
Eric Lee <ericlee@Erics-MacBook-Pro.local>
2025-10-10 10:22:12 -0700

    remove netlify stuff

--- test-api.js
+++
@@ -1,7 +0,0 @@
-// Simple test script to verify the stations data can be loaded
-const stations = require('./src/stations.js');
-
-console.log('Testing stations data...');
-console.log('Number of stations:', stations.default ? stations.default.length : stations.length);
-console.log('First station:', JSON.stringify(stations.default ? stations.default[0] : stations[0], null, 2));
-console.log('✅ Stations data loaded successfully!');
